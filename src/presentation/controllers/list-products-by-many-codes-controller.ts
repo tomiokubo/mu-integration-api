49980c861a300baac0c1758f5f40db841274c387
Tomio Kuboyama <viniciuskuboyama@gmail.com>
2024-07-02 16:19:17 -0300

    load by many codes validation removed

--- src/presentation/controllers/list-products-by-many-codes-controller.ts
+++ src/presentation/controllers/list-products-by-many-codes-controller.ts
@@ -1,7 +1,3 @@
-import {
-  ListProducts,
-  ListProductsFilterOptions,
-} from "@/domain/usecases/list-products";
 import { ListProductsByManyCodes } from "@/domain/usecases/list-products-by-many-codes";
 import { badRequest, noContent, ok, serverError } from "../helpers";
 import { Controller, HttpResponse } from "../protocols";
@@ -11,10 +7,10 @@ export class ListProductsByManyCodesController implements Controller {
   constructor(
     private readonly validation: Validation,
     private readonly listProducts: ListProductsByManyCodes
-  ) {}
+  ) { }
   async handle(request: { codes: string[] }): Promise<HttpResponse> {
-    const error = this.validation.validate(request);
-    if (error) return badRequest(error);
+    // const error = this.validation.validate(request);
+    // if (error) return badRequest(error);
     try {
       const products = await this.listProducts.list(request.codes);
       return ok(products);
